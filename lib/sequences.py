a43b677e7b8b820d41232a2cf0f869231bed605a
Jim Brennan <jim.j.brennan@gmail.com>
2023-02-20 13:58:25 -0500

    passed tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    if length > 0:
+        fib_list.append(0)
+        if length > 1:
+            fib_list.append(1)
+            if length > 2:
+                    for i in range(2, length):
+                         fib_list.append(fib_list[i - 1] + fib_list[i -2])
+    print(fib_list)
+
+
+
